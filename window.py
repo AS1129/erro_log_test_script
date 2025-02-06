6043f21b25e10ae4935bc4942cfa8fb005c7dcb1
AS1129 <popepo09@yahoo.co.jp>
2025-02-06 13:19:34 +0900

    ChatGPTと3時間格闘した結果。

---
+++ window.py
@@ -0,0 +1,18 @@
+# import pygetwindow
+
+# windows = pygetwindow.getAllWindows()
+# print(windows)
+import numpy as np
+
+
+kkk = "aaa"
+
+kkk = "bbb"
+
+kkk = "ccc"
+
+a = 1 + 2
+print(kkk)
+
+import tkinter
+print(tkinter.TkVersion)
